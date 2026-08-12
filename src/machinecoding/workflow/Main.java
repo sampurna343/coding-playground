cd27aeb9dbfc4bd915893055161d4e994e68fe8e
Sampurna <sampurna@Sampurnas-MacBook-Air.local>
2026-08-12 23:23:13 +0530

    workflow main

--- src/machinecoding/workflow/Main.java
+++ src/machinecoding/workflow/Main.java
@@ -7,34 +7,26 @@ import machinecoding.workflow.entity.Workflow;
 public class Main {
     public static void main(String[] args) {
 
-
         ApplyLeaveStep applyLeaveStep = new ApplyLeaveStep();
-        Condition isCreatedStatus = ((leaveObject) -> ((Leave) leaveObject).getStatus().equals(LeaveStatus.CREATED));
-
         SystemAcceptedLeaveStep systemAcceptedLeaveStep = new SystemAcceptedLeaveStep();
-        Condition isPendingStatus = ((leaveObject) -> ((Leave) leaveObject).getStatus().equals(LeaveStatus.PENDING));
-
         TakeActionForLeaveStep takeActionForLeaveStep = new TakeActionForLeaveStep();
-        Condition isApprovedStatus = ((leaveObject) -> ((Leave) leaveObject).getStatus().equals(LeaveStatus.APPROVED));
-        Condition isRejectedStatus = ((leaveObject) -> ((Leave) leaveObject).getStatus().equals(LeaveStatus.REJECTED));
-
         ApprovedLeaveStep approvedLeaveStep = new ApprovedLeaveStep();
         RejectedLeaveStep rejectedLeaveStep = new RejectedLeaveStep();
 
+        Condition isCreatedStatus = ((leaveObject) -> ((Leave) leaveObject).getStatus().equals(LeaveStatus.CREATED));
+        Condition isPendingStatus = ((leaveObject) -> ((Leave) leaveObject).getStatus().equals(LeaveStatus.PENDING));
+        Condition isApprovedStatus = ((leaveObject) -> ((Leave) leaveObject).getStatus().equals(LeaveStatus.APPROVED));
+        Condition isRejectedStatus = ((leaveObject) -> ((Leave) leaveObject).getStatus().equals(LeaveStatus.REJECTED));
 
         //Transitions
         Transition transitInto_SystemAcceptedLeaveStep = new Transition(isCreatedStatus, systemAcceptedLeaveStep);
-
         Transition transitInto_TakeActionForLeaveStep = new Transition(isPendingStatus, takeActionForLeaveStep);
-
         Transition transitInto_ApprovedLeaveStep = new Transition(isApprovedStatus, approvedLeaveStep);
         Transition transitInto_RejectedLeaveStep = new Transition(isRejectedStatus, rejectedLeaveStep);
 
         //Adding Transitions
         applyLeaveStep.addTransition(transitInto_SystemAcceptedLeaveStep);
-
         systemAcceptedLeaveStep.addTransition(transitInto_TakeActionForLeaveStep);
-
         takeActionForLeaveStep.addTransition(transitInto_ApprovedLeaveStep);
         takeActionForLeaveStep.addTransition(transitInto_RejectedLeaveStep);
 
